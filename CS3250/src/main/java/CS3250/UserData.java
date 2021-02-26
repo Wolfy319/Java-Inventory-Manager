4a9140de688b8cc42d129ea130d1ad5eca0f142a
James Marshall <slumburger15@gmail.com>
2021-02-25 21:08:16 -0800

    created user database object

---
+++ CS3250/src/main/java/CS3250/UserData.java
@@ -0,0 +1,55 @@
+package CS3250;
+
+import java.util.ArrayList;
+
+public class UserData implements DataInterface {
+
+    @Override
+    public ArrayList<Entry> getEntries() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void initializeDatabase(String filename) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void createEntry(String ID, Entry e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Entry readEntry(String ID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateEntry(String ID, Entry e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteEntry(String id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void saveEntry(Entry e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int retSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
