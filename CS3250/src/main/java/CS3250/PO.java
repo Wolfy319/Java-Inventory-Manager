f6248f9297b8c88b1b32442b9fb99af64ab5b81d
James Marshall <james_marshall48@aol.com>
2021-02-28 15:08:14 -0700

    Finished PO class

--- CS3250/src/main/java/CS3250/PO.java
+++ CS3250/src/main/java/CS3250/PO.java
@@ -8,7 +8,29 @@ public class PO {
     int userID = 0;
     List<Integer> itemPOs = new ArrayList<Integer>();
     String Username = "";
-    
+
+    public int getUserID() {
+        return userID;
+    }
+    public String getUsername() {
+        return Username;
+    }
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    public void setUsername(String username) {
+        Username = username;
+    }
+    public void setItemPOs(List<Integer> itemPOs) {
+        this.itemPOs = itemPOs;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public int getID() {
+        return ID;
+    }
+
 
     
 }
