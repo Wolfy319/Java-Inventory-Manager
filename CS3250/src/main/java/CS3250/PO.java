7a9ec53e6046e089e0b6ee2434bb64e1154b8db0
James Marshall <james_marshall48@aol.com>
2021-02-28 15:04:53 -0700

    started PO class need to make branch

---
+++ CS3250/src/main/java/CS3250/PO.java
@@ -0,0 +1,14 @@
+package CS3250;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PO {
+    int ID = 0;
+    int userID = 0;
+    List<Integer> itemPOs = new ArrayList<Integer>();
+    String Username = "";
+    
+
+    
+}
---
+++ CS3250/src/main/java/CS3250/POFactory.java
@@ -0,0 +1,20 @@
+package CS3250;
+
+import java.util.ArrayList;
+
+public class POFactory {
+    ArrayList<PO> arr = new ArrayList<PO>();
+    int userID = 0;
+    int itemID = 0;
+
+
+    public void setUserID(int id) {
+        userID = id;
+    }
+
+    public void setItemID(int ID){
+        itemID = ID;
+    }
+    
+    
+}
