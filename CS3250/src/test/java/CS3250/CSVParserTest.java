764ae4bfae361e69b0bad70b628b2991fc23bc58
Wolfy Fiorini <jonwolffiorini@gmail.com>
2021-04-01 21:22:53 -0700

    Added a parser for purchases. Doesn't work yet

--- CS3250/src/test/java/CS3250/CSVParserTest.java
+++ CS3250/src/test/java/CS3250/CSVParserTest.java
@@ -12,13 +12,18 @@ import CS3250.CSVParser;
 class CSVParserTest {
 	CSVParser parse = new CSVParser();
 	CSVData data = new CSVData();
-	String file = "inventory_team3.csv";
+	String file = "/customer_orders_team5.csv";
 
+	
+	// @Test
+	// public void ReadCSVWorks() {
+	// 	parse.readCSV(file, data);
+	// 	assertEquals(42585, data.retSize());
+	// }
 	
 	@Test
 	public void ReadCSVWorks() {
-		parse.readCSV(file, data);
-		assertEquals(42585, data.retSize());
+		parse.readOrdersCSV(file, data);
+		assert(true);
 	}
-
 }
