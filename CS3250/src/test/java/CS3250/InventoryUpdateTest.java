87908e7a20cbf594a29bf585d49b41d258896a0e
james-R-marshall <slumburger15@gmail.com>
2021-04-25 15:10:30 -0700

    created userDB which uses the dataman interface too. Also adjusted the user authenticator to work with it.

--- CS3250/src/test/java/CS3250/InventoryUpdateTest.java
+++ CS3250/src/test/java/CS3250/InventoryUpdateTest.java
@@ -21,7 +21,7 @@ public class InventoryUpdateTest {
     public void UpdateWorks() {
 		observablePO p = new observablePO();
 		p.setProductID("000TESTENTRY");
-		p.quantity(1);
+		p.quantity("" + 1);
 		p.setDate("2021-10-12");
 		p.setEmail("testemail@email.com");
 		p.setCustomerLocation("00000");
